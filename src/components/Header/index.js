625c744515316211d2b382f01aa69a92d699cc7a
mohamed1973465 <mr2837465@gmail.com>
2022-02-12 12:56:57 +0200

    last changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Header(props) {
   return (
-    <>
+    <div className="bg-red-100 text-red-500 text-2xl">
       {props.title.map((e, k) => (
         <h2 key={k}>{e}</h2>
       ))}
-    </>
+    </div>
   );
 }
 export default Header;
