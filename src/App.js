a4049df8231349e14fb32e1ce0fc7251906f1ab0
mohamed1973465 <mr2837465@gmail.com>
2022-02-12 11:44:57 +0200

    alter App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header title= {["mohamed","Ahmed","Hesham"]} />
-      <Content name = {[1,2,3,4,5]} />
+      <Header title={["mohamed", "Ahmed", "Hesham"]} />
+      <h1>this is content section</h1>
+      <Content name={[1, 2, 3, 4, 5]} />
       <Footer contactUs="any thing" />
     </>
   );
